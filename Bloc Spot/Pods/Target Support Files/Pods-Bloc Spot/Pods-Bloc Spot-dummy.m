332fe43f64d27c0c330826957331cfe5c73e618b
Kevin Thrailkill <kevinjt92@gmail.com>
2016-06-11 20:39:23 -0700

    First Commit - Setting up view controllers and data models

---
+++ Bloc Spot/Pods/Target Support Files/Pods-Bloc Spot/Pods-Bloc Spot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bloc_Spot : NSObject
+@end
+@implementation PodsDummy_Pods_Bloc_Spot
+@end
---
+++ Bloc Spot/Pods/Target Support Files/Pods-Bloc Spot/Pods-Bloc Spot-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Bloc_SpotVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Bloc_SpotVersionString[];
+
